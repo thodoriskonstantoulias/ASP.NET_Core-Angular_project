57e11d687f81989c5581051a890dfdac774da18e
Theodoros Konstantoulias <theodoros.konstantoulias@agileactors.com>
2019-10-01 21:59:55 +0300

    Addtion of sqlite database,seed,and check API with POSTMAN

--- Udemy_Web_API_core_angular_project/Startup.cs
+++ Udemy_Web_API_core_angular_project/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Udemy_Web_API_core_angular_project.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Udemy_Web_API_core_angular_project
 {
@@ -25,6 +27,7 @@ namespace Udemy_Web_API_core_angular_project
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
