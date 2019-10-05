d330a84e2d49faf58fccc1d0db9b190eac12f472
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-10-05 22:34:38 +0300

    Complete Login and Register funcionality

---
+++ Udemy_Web_API_core_angular_project/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Udemy_Web_API_core_angular_project.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(8,MinimumLength=4,ErrorMessage ="Password must be between 4 and 8 chars")]
+        public string Password { get; set; }
+    }
+}
